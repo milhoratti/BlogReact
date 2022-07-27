24ed20a706d6e078c38b347bc60fced7021794d6
Victor <victor.milhoratti@gmail.com>
2022-07-26 22:02:10 -0300

    fix: node blog-pessoal

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <p><img src="https://i.imgur.com/uWMbEIc.png" alt="Heisenberg" className="img" /></p>
+        </>
+    );
+}
+
+export default Home;
