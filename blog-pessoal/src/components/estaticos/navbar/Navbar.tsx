85de3725c55ca00294b68fa8661c628e625ae843
Victor <victor.milhoratti@gmail.com>
2022-07-27 21:15:15 -0300

    feat: NavBar/Footer

---
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,48 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+import React from "react";
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                logout
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+export default Navbar;
