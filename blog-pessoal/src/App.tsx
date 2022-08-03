c0d26a5c3c37f4334b7ee7fc4982bc3a33114154
Victor <victor.milhoratti@gmail.com>
2022-08-03 15:02:12 -0300

    feat: tema/postagem

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -6,6 +6,8 @@ import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 function App() {
   return (
@@ -14,10 +16,17 @@ function App() {
       <div style={{ minHeight: '100vh' }}>
         <Routes> // Antigo Switch
           <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
+
           <Route path="/home" element={<Home />} />
+
+          <Route path="/login" element={<Login />} />
+
           <Route path="/cadastrousuario" element={<CadastroUsuario />} />
-          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/posts" element={<ListaPostagem />} />
+
         </Routes>
       </div>
       <Footer />
